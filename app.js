d626408f0d9d2e7dd63d7689e49913bdb9553bf4
joessir <lassiryyass@gmail.com>
2023-02-20 17:47:27 +0100

    the app basic functionalities are all there, I may extend the app features in the future

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 class Board {
   constructor(state = ['', '', '', '', '', '', '', '', '']) {
     this.state = state;
@@ -55,6 +59,11 @@ class Board {
 
   insert(symbol, index) {
     this.state[index] = symbol;
+  }
+
+  resetBoard()
+  {
+    this.state = ['', '', '', '', '', '', '', '', '']
   }
 }
 
@@ -119,27 +128,125 @@ class AI {
 }
 
 class UserInterface {
-  static displayMove(symbol, elem, index) {
+  static  displayMove(symbol, elem) {
     elem.firstChild.textContent = symbol;
     elem.style.pointerEvents = 'none';
+  }
+
+  static async displayBoard(choice) {
+    const intro = document.querySelector('.intro');
+    const board = document.querySelector('.board');
+    const container = document.querySelector('.container');
+
+    if (choice == 'vs-ai')
+      intro.innerHTML =
+        '<h4>You are facing the unbeatable AI, good luck...</h4>';
+    else
+      intro.innerHTML =
+        '<h4>This is a player vs player mode, may the best win !</h4>';
+
+    container.classList.add('hidden');
+    intro.classList.remove('hidden');
+    await sleep(6000);
+    intro.classList.add('hidden');
+    board.classList.remove('hidden');
+  }
+
+  static displayWinner(win) {
+    const outro = document.querySelector('.outro');
+    if (win.winner == 'Draw')
+    outro.firstChild.nextSibling.textContent = 'This is a Draw'
+    else outro.firstChild.nextSibling.textContent = `${win.winner} has won`;
+    outro.classList.remove('hidden');
+  }
+
+  static resetBoard()
+  {
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach(cell => {
+      cell.firstChild.textContent = '';
+      cell.style.pointerEvents = 'all';
+    })
   }
 }
 
 (function Game() {
-  const player = new Player('X');
-  const ai = new AI('0');
+  let choice;
   const board = new Board();
-
   const cells = document.querySelectorAll('.cell');
-  cells.forEach((cell) => {
-    cell.addEventListener('click', (e) => {
-      const index = e.target.dataset.index;
-      board.insert(player.symbol, index);
-      UserInterface.displayMove(player.symbol, e.target, index);
-      const bestMoveIndex = ai.findBestMove(board);
-      board.insert(ai.symbol, bestMoveIndex);
-      const target = document.querySelector(`[data-index='${bestMoveIndex}'`);
-      UserInterface.displayMove(ai.symbol, target, bestMoveIndex);
-    });
+  const choiceBtns = document.querySelector('.btns');
+  const replayBtn = document.querySelector('.replay');
+
+  choiceBtns.addEventListener('click', (e) => {
+    if (e.target.type == 'button') {
+      choice = e.target.classList.value;
+      UserInterface.displayBoard(choice);
+      start();
+    }
   });
+
+  const start = () => {
+    if (choice == 'vs-player') {
+      const player1 = new Player('X');
+      const player2 = new Player('0');
+      let turn = 1;
+      cells.forEach((cell) => {
+        cell.addEventListener('click', (e) => {
+          if (turn) playerVSplayer(e, player1);
+          else playerVSplayer(e, player2);
+          turn = !turn;
+        });
+      });
+    } else {
+      const player = new Player('X');
+      const ai = new AI('0');
+      cells.forEach((cell) => {
+        cell.addEventListener('click', (e) => {
+          playerVSai(e, player, ai);
+        });
+      });
+    }
+  };
+
+  const playerVSai = async(e, player, ai) => {
+    const boardHTML = document.querySelector('.board');
+    const index = e.target.dataset.index;
+    let win;
+    board.insert(player.symbol, index);
+    UserInterface.displayMove(player.symbol, e.target);
+    win = board.checkWinner();
+    if (win) {
+      UserInterface.displayWinner(win);
+      boardHTML.style.pointerEvents = 'none';
+    }
+    const bestMoveIndex = ai.findBestMove(board);
+    board.insert(ai.symbol, bestMoveIndex);
+    const target = document.querySelector(`[data-index='${bestMoveIndex}'`);
+    await sleep(1000);
+    UserInterface.displayMove(ai.symbol, target);
+    win = board.checkWinner();
+    if (win) {
+      UserInterface.displayWinner(win);
+      boardHTML.style.pointerEvents = 'none';
+    }
+  };
+
+  const playerVSplayer = (e, player) => {
+    const boardHTML = document.querySelector('.board');
+    const index = e.target.dataset.index;
+    let win;
+    board.insert(player.symbol, index);
+    UserInterface.displayMove(player.symbol, e.target);
+    win = board.checkWinner();
+    if (win) {
+      UserInterface.displayWinner(win);
+      boardHTML.style.pointerEvents = 'none';
+    }
+  };
+
+  replayBtn.addEventListener('click', () => {
+    location.reload();
+  });
+
 })();
