55b5982551d32927558e22a0eb880390390095c1
joessir <lassiryyass@gmail.com>
2023-03-11 15:19:07 +0100

    added alpha beta pruning to optimize the minimax algorithm

--- Unbeatable-TicTacToe/app.js
+++ Unbeatable-TicTacToe/app.js
@@ -73,7 +73,9 @@ class AI {
     this.symbol = symbol;
   }
 
-  minimax(board, depth, isMaximising) {
+  // alpha : represents the best alternative for Max on this particular path of the tree
+  // beta : the best alternative for the Min
+  minimax(board, depth, isMaximising,alpha,beta) {
     const win = board.checkWinner();
     if (win) {
       const playerSymbol = this.symbol == 'X' ? '0' : 'X';
@@ -83,23 +85,35 @@ class AI {
     }
 
     if (isMaximising) {
-      let bestScore = -1000;
+      let bestScore = -Infinity;
       for (let i = 0; i < 9; i++) {
         if (board.state[i] != '') continue;
         board.state[i] = this.symbol;
-        const score = this.minimax(board, depth + 1, !isMaximising);
+        const score = this.minimax(board, depth + 1, !isMaximising,alpha,beta);
         board.state[i] = '';
         bestScore = Math.max(score, bestScore);
+        if (alpha === undefined || alpha < score) alpha = score;
+        if (beta && score > beta) return bestScore;
+        // When we are maximising and we find a value that is greater
+        // than beta , it means no need to continue the search because
+        // the min function will choose the minimum , which is the other part
+        // of the tree
       }
       return bestScore;
     } else {
-      let bestScore = 1000;
+      let bestScore = Infinity;
       for (let i = 0; i < 9; i++) {
         if (board.state[i] != '') continue;
         board.state[i] = this.symbol == 'X' ? '0' : 'X';
-        const score = this.minimax(board, depth + 1, !isMaximising);
+        const score = this.minimax(board, depth + 1, !isMaximising,alpha,beta);
         board.state[i] = '';
         bestScore = Math.min(score, bestScore);
+        if (beta === undefined || beta > score) beta = score;
+        if (alpha && score < alpha) return bestScore;
+        // when we are minimising (looking fo the min value ) and we find a value that is less
+        // than alpha , it means that when we go up the tree and we maximise , we are going to choose the
+        // part of the tree that is the maximum , which is the one that has the value of alpha
+      
       }
       return bestScore;
     }
@@ -107,11 +121,13 @@ class AI {
 
   findBestMove(board) {
     let position;
-    let bestScore = -1000;
+    let bestScore = -Infinity;
+    let alpha = undefined;
+    let beta = undefined;
     for (let i = 0; i < 9; i++) {
       if (board.state[i] != '') continue;
       board.state[i] = this.symbol;
-      const score = this.minimax(board, 0, false);
+      const score = this.minimax(board, 0, false,alpha,beta);
       board.state[i] = '';
       if (score > bestScore) {
         bestScore = score;
@@ -224,7 +240,7 @@ class UserInterface {
     board.insert(ai.symbol, bestMoveIndex);
     const target = document.querySelector(`[data-index='${bestMoveIndex}'`);
     boardHTML.style.pointerEvents = 'none';
-    await sleep(1000);
+    await sleep(500);
     boardHTML.style.pointerEvents = 'all';
     UserInterface.displayMove(ai.symbol, target);
     win = board.checkWinner();
